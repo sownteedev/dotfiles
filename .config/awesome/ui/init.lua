7c8c302ac4a36acda5589af48d09d762bbdb2657
sownteedev <sownteenguyen@gmail.com>
2023-11-27 05:17:13 +0700

    :>

--- .config/awesome/ui/init.lua
+++ .config/awesome/ui/init.lua
@@ -4,8 +4,4 @@ require("ui.notifs_center")
 require("ui.calendar")
 require("ui.titlebars")
 require("ui.control")
-<<<<<<< HEAD
--- require("ui.mplayer")
-=======
 require("ui.mplayer")
->>>>>>> temp-branch
