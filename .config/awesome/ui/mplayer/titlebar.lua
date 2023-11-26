df0624f4c0744902fad88e9662641ff6b84f7c0a
sownteedev <sownteenguyen@gmail.com>
2023-11-27 05:09:49 +0700

    :>

--- .config/awesome/ui/mplayer/titlebar.lua
+++ .config/awesome/ui/mplayer/titlebar.lua
@@ -2,11 +2,11 @@ local beautiful = require('beautiful')
 local wibox = require('wibox')
 local dpi = beautiful.xresources.apply_dpi
 
-local helpers = require('helpers')
+local helpers = require('helpers.mplayer')
 
-local close = helpers.textbox(beautiful.red, "Ubuntu nerd font bold 16", "  ")
-local pin_top = helpers.textbox(beautiful.green, "Ubuntu nerd font bold 16", "  ")
-local pin_bottom = helpers.textbox(beautiful.yellow, "Ubuntu nerd font bold 16", "  ")
+local close = helpers.textbox(beautiful.red, "Liga SFMono Nerd Font bold 10", " ")
+local pin_top = helpers.textbox(beautiful.green, "Liga SFMono Nerd Font bold 10", " ")
+local pin_bottom = helpers.textbox(beautiful.yellow, "Liga SFMono Nerd Font bold 10", " ")
 
 local add_signal = function(button, signal)
 	button:connect_signal("button::release", function()
@@ -21,7 +21,7 @@ add_signal(pin_bottom, "mplayer::pin_bottom")
 local titlebar = wibox.widget {
 	{
 		{
-			helpers.textbox(beautiful.blue, "Ubuntu nerd font bold 13", "Music Player"),
+			helpers.textbox(beautiful.blue, "Liga SFMono Nerd Font bold 10", "Music Player"),
 			nil,
 			{ pin_top, pin_bottom, close, layout = wibox.layout.fixed.horizontal },
 			layout = wibox.layout.align.horizontal
