c20f2690d18b0ca2a078ea0462b6dc01521cc2c7
sownteedev <sownteenguyen@gmail.com>
2023-12-21 02:36:12 +0700

    :>

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,11 +1,20 @@
 pcall(require, "luarocks.loader")
 require("awful.autofocus")
 local awful = require("awful")
+local gears = require("gears")
 local beautiful = require("beautiful")
 
-awful.spawn.with_shell("~/.config/awesome/autostart")
-beautiful.init("~/.config/awesome/themes/theme.lua")
+awful.spawn.with_shell("~/.config/awesome/config/autostart")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/theme.lua")
+
+local naughty = require "naughty"
+naughty.connect_signal("request::display_error", function(message, startup)
+	naughty.notification {
+		urgency = "critical",
+		title   = "Oops, an error happened" .. (startup and " during startup!" or "!"),
+		message = message
+	}
+end)
 
 require("config")
-require("modules")
 require("ui")
