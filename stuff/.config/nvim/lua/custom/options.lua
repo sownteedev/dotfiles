48097e50994e9ee764daabca142bd7b995dbe3ec
sownteedev <sownteenguyen@gmail.com>
2024-07-30 01:57:09 +0700

    :>

---
+++ stuff/.config/nvim/lua/custom/options.lua
@@ -0,0 +1,11 @@
+-- add options or override my options in here
+vim.g.TeVimTheme = "one_light"
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+vim.g.transparency = false
+vim.g.loadTeDash = false
+vim.g.loadTeBufLine = true
+vim.g.loadTeStatusLine = true
+vim.g.loadTeVimTheme = true
+vim.g.snipmate_snippets_path = vim.fn.stdpath("config") .. "/lua/custom/snippets"
