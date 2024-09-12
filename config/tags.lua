05490c93ab60820072ec481960dd4ae3971b2a2b
sownteedev <sownteenguyen@gmail.com>
2024-09-13 01:16:14 +0700

    :>

--- config/tags.lua
+++ config/tags.lua
@@ -1,7 +1,6 @@
 local awful = require("awful")
 local gears = require("gears")
 local beautiful = require("beautiful")
-local naughty = require("naughty")
 
 awful.layout.layouts = {
 	awful.layout.suit.floating,
@@ -40,3 +39,32 @@ screen.connect_signal("request::desktop_decoration", function(s)
 end)
 
 gears.wallpaper.maximized(beautiful.wallpaper)
+
+---@diagnostic disable: need-check-nil
+awesome.connect_signal("exit", function(reason_restart)
+	if not reason_restart then
+		return
+	end
+	local file = io.open("/tmp/awesomewm-last-selected-tags", "w+")
+	for s in screen do
+		file:write(s.selected_tag.index, "\n")
+	end
+	file:close()
+end)
+
+awesome.connect_signal("startup", function()
+	local file = io.open("/tmp/awesomewm-last-selected-tags", "r")
+	if not file then
+		return
+	end
+	local selected_tags = {}
+	for line in file:lines() do
+		table.insert(selected_tags, tonumber(line))
+	end
+	for s in screen do
+		local i = selected_tags[s.index]
+		local t = s.tags[i]
+		t:view_only()
+	end
+	file:close()
+end)
