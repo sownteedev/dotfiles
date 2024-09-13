5ead388084a6bd614ac954865a3c7ba25182b445
sownteedev <sownteenguyen@gmail.com>
2024-09-13 13:10:43 +0700

    :>

--- widgets/control/mods/button.lua
+++ widgets/control/mods/button.lua
@@ -15,68 +15,89 @@ end
 
 local getName = function(type)
 	---@diagnostic disable: param-type-mismatch
+	local string = ""
 	if type == "rec" then
-		local string = "~/Videos/Recordings/" .. os.date("%d-%m-%Y-%H:%M:%S") .. ".mp4"
-		string = string:gsub("~", os.getenv("HOME"))
-		return string
+		string = "~/Videos/Recordings/" .. os.date("%d-%m-%Y-%H:%M:%S") .. ".mp4"
 	elseif type == "screenshot" then
-		local string = "~/Pictures/Screenshots/" .. os.date("%d-%m-%Y-%H:%M:%S") .. ".png"
-		string = string:gsub("~", os.getenv("HOME"))
-		return string
+		string = "~/Pictures/Screenshots/" .. os.date("%d-%m-%Y-%H:%M:%S") .. ".png"
 	end
+	string = string:gsub("~", os.getenv("HOME"))
+	return string
 end
 
-local function with_defaults(given_opts)
-	return { notify = given_opts == nil and false or given_opts.notify }
-end
+local function do_notify(which, tmp_path)
+	if which == "screenshot" then
+		local open = naughty.action({ name = "Open" })
+		local delete = naughty.action({ name = "Delete" })
 
-local function do_notify(tmp_path)
-	local open = naughty.action({ name = "Open" })
-	local delete = naughty.action({ name = "Delete" })
+		open:connect_signal("invoked", function()
+			awful.spawn.with_shell('xclip -sel clip -target image/png "' ..
+				tmp_path .. '" && viewnior "' .. tmp_path .. '"')
+			naughty.notify({
+				app_name = "Screenshot",
+				app_icon = tmp_path,
+				icon = tmp_path,
+				title = "Screenshot",
+				text = "Screenshot copied successfully.",
+			})
+		end)
 
-	open:connect_signal("invoked", function()
-		awful.spawn.with_shell('xclip -sel clip -target image/png "' ..
-			tmp_path .. '" && viewnior "' .. tmp_path .. '" &')
+		delete:connect_signal("invoked", function()
+			awful.spawn.with_shell('xclip -sel clip -target image/png "' ..
+				tmp_path .. '" && rm -f "' .. tmp_path .. '"')
+			naughty.notify({
+				app_name = "Screenshot",
+				title = "Screenshot",
+				text = "Screenshot copied and deleted successfully.",
+			})
+		end)
+
 		naughty.notify({
 			app_name = "Screenshot",
 			app_icon = tmp_path,
 			icon = tmp_path,
 			title = "Screenshot",
-			text = "Screenshot copied successfully.",
+			text = "Screenshot saved successfully",
+			actions = {
+				open,
+				delete,
+			},
 		})
-	end)
+	elseif which == "rec" then
+		local open = naughty.action({ name = "Open" })
+		local delete = naughty.action({ name = "Delete" })
 
-	delete:connect_signal("invoked", function()
-		awful.spawn.with_shell('xclip -sel clip -target image/png "' ..
-			tmp_path .. '" && rm -f "' .. tmp_path .. '" &')
+		open:connect_signal("invoked", function()
+			awful.spawn.with_shell('xdg-open "' .. tmp_path .. '"')
+		end)
+
+		delete:connect_signal("invoked", function()
+			awful.spawn.with_shell('rm -f "' .. tmp_path .. '" &')
+			naughty.notify({
+				app_name = "Recording",
+				title = "Recording",
+				text = "Recording deleted successfully.",
+			})
+		end)
+
 		naughty.notify({
-			app_name = "Screenshot",
-			title = "Screenshot",
-			text = "Screenshot copied and deleted successfully.",
+			app_name = "Recording",
+			title = "Recording",
+			text = "Recording saved successfully",
+			actions = {
+				open,
+				delete,
+			},
 		})
-	end)
-
-	naughty.notify({
-		app_name = "Screenshot",
-		app_icon = tmp_path,
-		icon = tmp_path,
-		title = "Screenshot",
-		text = "Screenshot saved successfully",
-		actions = {
-			open,
-			delete,
-		},
-	})
+	end
 end
 
-function area(opts)
+function area()
 	checkFolder("screenshot")
 	local tmp_path = getName("screenshot")
-	awful.spawn.easy_async_with_shell('sleep 1.5 && maim --select "' .. tmp_path .. '" &', function()
+	awful.spawn.easy_async_with_shell('sleep 1.5 && maim --select "' .. tmp_path .. '"', function()
 		awesome.emit_signal("screenshot::done")
-		if with_defaults(opts).notify then
-			do_notify(tmp_path)
-		end
+		do_notify("screenshot", tmp_path)
 	end)
 end
 
@@ -85,17 +106,23 @@ function record()
 	local display = os.getenv("DISPLAY")
 	local name = getName("rec")
 	local defCommand = string.format(
-		"sleep 1.25 && ffmpeg -y -f x11grab "
-		.. "-r 60 -i %s -f pulse -i %s -c:v libx264 -qp 0 -profile:v main "
-		.. "-preset ultrafast -tune zerolatency -crf 28 -pix_fmt yuv420p "
-		.. "-c:a aac -b:a 64k -b:v 500k %s &",
+		"sleep 1.25 && ffmpeg -y -f x11grab -r 60 "
+		-- display and audio
+		.. "-i %s -f pulse -i %s "
+		-- video
+		.. "-c:v libx264 -crf 23 -preset fast -pix_fmt yuv420p "
+		-- audio
+		.. "-c:a libvorbis -b:a 128k -b:v 10000k %s",
 		display,
 		speaker_input,
 		name
 	)
 	print(defCommand)
 	checkFolder("rec")
-	awful.spawn.with_shell(defCommand)
+	awful.spawn.easy_async_with_shell(defCommand, function()
+		awesome.emit_signal("recording::done")
+		do_notify("rec", name)
+	end)
 end
 
 local createButton = function(name, img, cmd1, cmd2)
@@ -146,8 +173,7 @@ local createButton = function(name, img, cmd1, cmd2)
 end
 
 local button = wibox.widget({
-	createButton("Screenshot", beautiful.icon_path .. "button/screenshot.svg",
-		"awesome-client 'area({ notify = true })' &", ""),
+	createButton("Screenshot", beautiful.icon_path .. "button/screenshot.svg", "awesome-client 'area()' &", ""),
 	createButton("Record", beautiful.icon_path .. "button/record.svg", "awesome-client 'record()' &", "killall ffmpeg &"),
 	spacing = 15,
 	layout = wibox.layout.fixed.horizontal,
