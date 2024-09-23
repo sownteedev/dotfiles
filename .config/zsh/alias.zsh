34362d54e5a86b33f0a73d32045041ad2ca854ec
sownteedev <sownteenguyen@gmail.com>
2024-09-24 00:31:15 +0700

    :>

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,14 +1,12 @@
 if [ $(command -v exa) ]; then
     alias ll="exa --all --long --icons --group --git"
-    alias ls="exa"
-    alias la="exa --long --all --group"
+    alias ls="exa --icons"
 fi
 
 if [ $(command -v ripgrep) ]; then
     alias grep="ripgrep"
 fi
 
-alias rel="xrdb merge ~/.Xresources && kill -USR1 $(pidof st)"
 alias cls="clear"
 alias nvide="neovide"
 
@@ -26,13 +24,7 @@ alias mv='mv -i'
 
 alias dotpush='git add . && git commit -m ":>" && git push'
 alias syncfont='sudo fc-cache -fv'
-alias windows='sudo mount -t ntfs-3g -o ro /dev/nvme0n1p2 $HOME/Windows'
-alias startx='startx -- -keeptty >~/.xorg.log 2>&1'
-alias fetch="$HOME/.local/bin/fetch"
-alias trans="$HOME/.local/bin/trans"
-alias wal="$HOME/.local/bin/wal"
 
-bindkey -s ^n "nvims\n"
 bindkey -s ^o "startx\n"
 bindkey -s ^w "sudo mount -t ntfs-3g -o ro /dev/nvme0n1p2 $HOME/Windows\n"
 bindkey '^e' "autosuggest-accept"
