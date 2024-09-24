ad78ba6f0ad68f8d007857e8fe594c81efd210c5
sownteedev <sownteenguyen@gmail.com>
2024-09-25 02:27:10 +0700

    :>

--- widgets/exit/init.lua
+++ widgets/exit/init.lua
@@ -3,6 +3,9 @@ local wibox = require("wibox")
 local gears = require("gears")
 local beautiful = require("beautiful")
 local helpers = require("helpers")
+local animation = require("modules.animation")
+
+local exit, slide, slide_end, prompt_grabber
 
 local createButton = function(icon, name, cmd)
 	local button = wibox.widget({
@@ -37,19 +40,64 @@ local createButton = function(icon, name, cmd)
 		bg = helpers.change_hex_lightness(beautiful.background, 4),
 		shape = beautiful.radius,
 		widget = wibox.container.background,
-		buttons = awful.button({}, 1, function()
-			awful.spawn(cmd)
-		end),
+		buttons = {
+			awful.button({}, 1, function()
+				prompt_grabber:stop()
+				exit.visible = false
+				awful.spawn(cmd)
+			end),
+		},
 	})
-	helpers.addHoverBg(button, "bg", helpers.change_hex_lightness(beautiful.background, 4),
-		helpers.change_hex_lightness(beautiful.background, 6))
+	button.cmd = cmd
 	helpers.hoverCursor(button)
-
 	return button
 end
 
+local entries_container = wibox.widget({
+	spacing = 15,
+	layout = wibox.layout.fixed.vertical,
+})
+
+local buttons = {
+	createButton("power/poweroff.svg", "Shutdown", "poweroff"),
+	createButton("power/restart.svg", "Reboot", "reboot"),
+	createButton("power/lock.svg", "Lock", "awesome-client \"awesome.emit_signal('toggle::lock')\""),
+	createButton("power/suspend.svg", "Suspend", "systemctl suspend"),
+	createButton("power/logout.svg", "Logout", "loginctl kill-user $USER"),
+}
+
+local index_entry = 1
+local function filter_entries()
+	entries_container:reset()
+	for i, button in ipairs(buttons) do
+		if i == index_entry then
+			helpers.gc(button, "bg").bg = helpers.change_hex_lightness(beautiful.background, 8)
+		else
+			helpers.gc(button, "bg").bg = helpers.change_hex_lightness(beautiful.background, 4)
+		end
+		entries_container:add(button)
+	end
+end
+
+local function next()
+	if index_entry < #buttons then
+		index_entry = index_entry + 1
+	else
+		index_entry = 1
+	end
+	filter_entries()
+end
+local function prev()
+	if index_entry > 1 then
+		index_entry = index_entry - 1
+	else
+		index_entry = #buttons
+	end
+	filter_entries()
+end
+
 return function(s)
-	local exit = wibox({
+	exit = wibox({
 		screen = s,
 		width = 250,
 		height = 380,
@@ -68,15 +116,7 @@ return function(s)
 				font = beautiful.sans .. " Medium 12",
 				widget = wibox.widget.textbox,
 			},
-			{
-				createButton("power/poweroff.svg", "Shutdown", "poweroff"),
-				createButton("power/restart.svg", "Reboot", "reboot"),
-				createButton("power/lock.svg", "Lock", "awesome-client \"awesome.emit_signal('toggle::lock')\" &"),
-				createButton("power/suspend.svg", "Suspend", "systemctl suspend"),
-				createButton("power/logout.svg", "Logout", "loginctl kill-user $USER"),
-				layout = wibox.layout.fixed.vertical,
-				spacing = 15,
-			},
+			entries_container,
 			layout = wibox.layout.fixed.vertical,
 		},
 		widget = wibox.container.margin,
@@ -86,6 +126,58 @@ return function(s)
 	awesome.connect_signal("signal::uptime", function(v)
 		helpers.gc(exit, "uptime").markup = helpers.colorizeText("Up: " .. v, beautiful.foreground)
 	end)
-	helpers.slideAnimation("toggle::exit", "close::exit", "left", exit, -exit.width, beautiful.useless_gap * 2)
+
+	prompt_grabber = awful.keygrabber({
+		auto_start = true,
+		stop_event = "release",
+		keypressed_callback = function(self, _, key, _)
+			if key == "Up" then
+				prev()
+			elseif key == "Down" or key == "Tab" then
+				next()
+			elseif key == "Return" then
+				self:stop()
+				exit.visible = false
+				gears.timer.start_new(0.2, function()
+					awful.spawn(buttons[index_entry].cmd)
+				end)
+			elseif key == "Escape" then
+				self:stop()
+				slide_end:start()
+				slide:set(-exit.width)
+			end
+		end,
+	})
+
+	slide = animation:new({
+		duration = 0.5,
+		pos = -exit.width,
+		easing = animation.easing.inOutExpo,
+		update = function(_, poss)
+			exit.x = poss
+		end,
+	})
+	slide_end = gears.timer({
+		timeout = 1,
+		single_shot = true,
+		callback = function()
+			exit.visible = false
+		end,
+	})
+
+	awesome.connect_signal("toggle::exit", function()
+		if exit.visible then
+			slide_end:start()
+			slide:set(-exit.width)
+			prompt_grabber:stop()
+		else
+			index_entry = 1
+			filter_entries()
+			prompt_grabber:start()
+			exit.visible = true
+			slide:set(beautiful.useless_gap * 2)
+		end
+	end)
+
 	return exit
 end
