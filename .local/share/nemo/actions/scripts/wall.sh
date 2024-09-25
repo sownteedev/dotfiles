44549474ca75348d4f269fa194123e838186bbc4
sownteedev <sownteenguyen@gmail.com>
2024-09-26 02:33:33 +0700

    :>

--- .local/share/nemo/actions/scripts/wall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-type=$(echo 'beautiful = require("beautiful"); return beautiful.type' | awesome-client | sed -e 's/string //' -e 's/"//g' | xargs)
-ESC_WP_PATH=$(printf %q "$1")
-awesome-client "changewall(\"$ESC_WP_PATH\", \"$type\")"
