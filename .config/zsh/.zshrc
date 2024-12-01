d448c446ce7193107e04cfba47fcb8a35aa94763
sownteedev <sownteenguyen@gmail.com>
2024-12-02 02:17:43 +0700

    :>

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -5,15 +5,11 @@ do
   source "$HOME/.config/zsh/$file.zsh"
 done <<-EOF
 plugs
-theme
 alias
 opts
 prompt
 EOF
 
-[ -f "$greet_path" ] && eval "$greet_path" || default_greeter
-
 export PATH=$PATH:~/.local/share/nvim/mason/bin
-
 export VISUAL=nvim;
 export EDITOR=nvim;
--- .config/zsh/opts.zsh
+++ .config/zsh/opts.zsh
@@ -1,6 +1,3 @@
-zstyle ':completion:*:git-checkout:*' sort false
-zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 zstyle ':fzf-tab:*' switch-group ',' '.'
 
